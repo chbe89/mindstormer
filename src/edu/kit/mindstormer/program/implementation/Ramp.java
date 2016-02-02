a17f1eb8bde4205a6fc2ac39ff2f17753f3431f4
Andreas M <andi_miko@yahoo.de>
2016-02-02 14:38:15 +0100

    NavigatorProgram
    NavigatorProgram da shiat

---
+++ src/edu/kit/mindstormer/program/implementation/Ramp.java
@@ -0,0 +1,14 @@
+package edu.kit.mindstormer.program.implementation;
+
+import edu.kit.mindstormer.program.AbstractProgram;
+
+public class Ramp extends AbstractProgram {
+
+	protected Ramp() {
+		super("Ramp");
+	}
+	
+	public void run() {
+		
+	}
+}
