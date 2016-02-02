fe38dceb22c6bb500ac23d46a8f9542bffe14298
Mikael Beyene <mikael@beyene.org>
2016-02-02 13:34:33 +0100

    Add global wrapper

---
+++ src/edu/kit/mindstormer/program/AbstractProgram.java
@@ -0,0 +1,31 @@
+package edu.kit.mindstormer.program;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public abstract class AbstractProgram implements Program {
+
+	private final String name;
+	protected final AtomicBoolean quit = new AtomicBoolean();
+
+	protected AbstractProgram(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void initialize() {
+		quit.set(false);
+	}
+
+	@Override
+	public abstract void run();
+
+	@Override
+	public final String getName() {
+		return name;
+	}
+
+	@Override
+	public void terminate() {
+		quit.set(true);
+	}
+}
