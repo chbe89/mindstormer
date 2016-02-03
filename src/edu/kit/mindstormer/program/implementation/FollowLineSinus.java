4ac1bfc8afbdb53141dd6ea6939a097c27435e24
Andreas M <andi_miko@yahoo.de>
2016-02-03 09:51:10 +0100

    FollowLineSinus mod

--- src/edu/kit/mindstormer/program/implementation/FollowLineSinus.java
+++ src/edu/kit/mindstormer/program/implementation/FollowLineSinus.java
@@ -22,46 +22,21 @@ public class FollowLineSinus extends AbstractProgram {
 	}
 	
 	public void run() {
-		int turnMultiplicator = 1;
 		boolean turnDirection = true;
 		while (!quit.get()) {
-			Complex.driveCurve(true, 2 ,500);
+			Complex.driveCurve(turnDirection, 2000, 500);
 			
+			waitForFoundLine();
+			turnDirection = !turnDirection;
+			Complex.driveCurve(turnDirection, 2000, 500);
 			
-			
-			boolean found1 = find((turnDirection ? 1 : -1) * turnMultiplicator * searchAngle);
-			boolean found2 = false;
-			if (!found1) {
-				found2 = find((turnDirection ? -1 : 1) * turnMultiplicator * 2 * searchAngle);
-			}
-			
-			if (found1 || found2) {
-				turnMultiplicator = 1;
-				if (found2) {
-					turnDirection = !turnDirection;
-				}
-			} else {
-				turnMultiplicator++;
-			}
-			
-		    
-		    Movement.move(forwardSpeed, forwardSpeed);
-		    while (sample[0] >= Constants.LINE_COLOR_THRESHOLD) {
-		    	sensor.fetchSample(sample, 0);
-		    }
 		    Movement.stop();
 		}
 	}
 	
-	private boolean find(float angle) {
-		Complex.rotate(angle, turnSpeed);
+	private void waitForFoundLine() {
 		while (sample[0] < Constants.LINE_COLOR_THRESHOLD && !State.stopped(true, true)) {
 	    	sensor.fetchSample(sample, 0);
 	    }
-		if (sample[0] >= Constants.LINE_COLOR_THRESHOLD) {
-			Movement.stop();
-			return true;
-		}
-		return false;
 	}
 }
